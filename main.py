001d394111f3a0e61e3445c80ac50e2b85d9ec36
Eryk <eryk.nejfert@wp.pl>
2025-01-12 19:25:52 +0100

    browsing data

--- main.py
+++ main.py
@@ -3,31 +3,72 @@ import tkinter
 from tkinter import filedialog, Menu
 from tkinter import messagebox
 from tkinter import *
+from tkinter import ttk
 import sqlite3
+import xml.etree.ElementTree as ET
 
 sqlite = None  
 kursor = None  
 
+def switch_tables(event):
+    global combo, sqlite, kursor, label_browse_data
+    
+    selection = combo.get()
+    query = f"SELECT * FROM {selection}"
+    
+    result = sqlite.execute(query).fetchall()
+    columns = [description[1] for description in sqlite.execute(f"PRAGMA table_info({selection})").fetchall()]
+    
+    print(columns)
+    
+    children = label_browse_data.winfo_children()
+    
+    # Destroy the second child (if it exists)
+    if len(children) > 1:
+        children[1].destroy()
+    
+    tree = ttk.Treeview(label_browse_data)
+    tree["columns"] = columns
+    tree["show"] = "headings"
+    
+    # Set the column headings and width
+    for col in columns:
+        tree.heading(col, text=col)
+        tree.column(col, width=100, anchor="center")
+    
+    # Insert the data from the query result into the Treeview
+
+    for row in result:
+        tree.insert('', 'end', values=row)
+        
+    tree.pack(expand=True, fill='both') 
+    
+
+    
+
 def switch_view(view):
-    global m, nav_db_struct, nav_browse_data, nav_sql, label_sql
+    global m, nav_db_struct, nav_browse_data, nav_sql, label_sql, label_browse_data
     match(view):
         case "1":
             nav_db_struct.config(bg=m.cget("bg"))
             nav_browse_data.config(bg='lightgray')
             nav_sql.config(bg='lightgray')
             
+            label_browse_data.pack_forget()
             label_sql.pack_forget()
         case "2":
             nav_db_struct.config(bg='lightgray')
             nav_browse_data.config(bg=m.cget("bg"))
             nav_sql.config(bg='lightgray')
             
+            label_browse_data.pack()
             label_sql.pack_forget()
-        case "3":
+        case "3":    
             nav_db_struct.config(bg='lightgray')
             nav_browse_data.config(bg='lightgray')
             nav_sql.config(bg=m.cget("bg"))
             
+            label_browse_data.pack_forget()
             label_sql.pack(fill='x', pady=(2, 0))  # 2px margin above the frame
     
 def create_db_from_sql(sql_file):
@@ -52,7 +93,7 @@ def create_db_from_sql(sql_file):
         
 
 def import_db():
-    global sqlite, kursor, inputtxt, printButton, m
+    global sqlite, kursor, inputtxt, printButton, combo, m
 
     db_path = filedialog.askopenfilename(
         title="Select a .db file",
@@ -63,12 +104,16 @@ def import_db():
         try:
             sqlite = sqlite3.connect(db_path)
             kursor = sqlite.cursor()
-            kursor.execute("PRAGMA schema_version;") # execute test
-            
-            # Show the Text box and Button only if the connection is successful
+            result = kursor.execute("SELECT name FROM sqlite_master WHERE type='table'")
+            tables = [str(table[0]) for table in result.fetchall()]
+
+            combo.set("Select table")
+            combo.config(values=tables)
             m.title("SQLite Database Manager - " + str(db_path))
+            
             inputtxt.pack()
             printButton.pack()
+            combo.pack()
         except sqlite3.Error as e:
             messagebox.showerror("Error", f"Error connecting to database: {e}")
             import_db()
@@ -78,13 +123,15 @@ def import_db_from_sql():
 
     db_path = filedialog.askopenfilename(
         title="Select a .sql file",
-        filetypes=[("SQLite Database", "*.sql")]
+        filetypes=[("SQL Database", "*.sql")]
     )
     
     if db_path:  
         try:
             create_db_from_sql(db_path)
-            kursor.execute("PRAGMA schema_version;") # execute test
+            result = kursor.execute("SELECT sql FROM sqlite_master WHERE type='table';") # execute test
+            
+            print(result.fetchall())
             
             # Show the Text box and Button only if the connection is successful
             m.title("SQLite Database Manager - " + str(db_path))
@@ -93,6 +140,8 @@ def import_db_from_sql():
         except sqlite3.Error as e:
             messagebox.showerror("Error", f"Error connecting to database: {e}")
             import_db_from_sql()       
+
+                        
      
 
 def execute_query():
@@ -120,7 +169,6 @@ importmenu = Menu(menuBar, tearoff=0)
 
 database_submenu = Menu(importmenu, tearoff=0)
 database_submenu.add_command(label="From SQL File", command=import_db_from_sql)
-database_submenu.add_command(label="From XML File")
 
 importmenu.add_cascade(label="Database", menu=database_submenu)
 
@@ -150,9 +198,16 @@ nav_sql = tkinter.Button(navbar, text="Execute SQL", borderwidth=0, relief="flat
 nav_sql.pack(side="left")
 
 label_sql = tkinter.Frame(m, padx=5, pady=5)
+label_browse_data = tkinter.Frame(m, padx=5, pady=5)
 
 
 inputtxt = tkinter.Text(label_sql, height=5, width=50)
 printButton = tkinter.Button(label_sql, text="Enter Query", command=execute_query)
+
+combo = ttk.Combobox(
+    label_browse_data,
+    state="readonly"
+)
+combo.bind("<<ComboboxSelected>>", switch_tables)
 
 m.mainloop()
