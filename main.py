178c1f352be778e7bf0d1b4acf9194b3c6c17dd8
Eryk <eryk.nejfert@wp.pl>
2025-01-12 13:56:36 +0100

    commit main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import tkinter
 from tkinter import ttk
 from tkinter import filedialog
+from tkinter import Menu
 
 import sqlite3
+
+sqlite = sqlite3.connect('db.db')
 
 
 def import_db():
@@ -15,60 +18,25 @@ def import_db():
     if db_path:  
         try:
             # Connect to the SQLite database
-            conn = sqlite3.connect(db_path)
-            cursor = conn.cursor()
-            
-            cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-            tables = cursor.fetchall()
-            
-            conn.close()
+            sqlite = sqlite3.connect(db_path)
+            if(sqlite):
+                print("Successfuly connected with database")
         except sqlite3.Error as e:
             print(f"Error connecting to database: {e}")
 
 
 m = tkinter.Tk()
 m.title("Sqlite test")
+m.geometry("1000x500")
 
-sqlite = sqlite3.connect('sql.sql')
+navbar = Menu(m)
+filemenu = Menu(navbar, tearoff=0)
+filemenu.add_command(label="Open Database", command=import_db)
+navbar.add_cascade(label="File", menu=filemenu)
+
+
+m.config(menu=navbar)
 kursor = sqlite.cursor()
 
-query = "SELECT * FROM users;"
-
-kursor.execute('''
-    CREATE TABLE IF NOT EXISTS users (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        username TEXT NOT NULL,
-        email TEXT NOT NULL,
-        age INTEGER
-    );
-''')
-
-kursor.executemany('''
-    INSERT INTO users (username, email, age)
-    VALUES (?, ?, ?)
-''', [
-    ('Alice', 'alice@example.com', 30),
-    ('Bob', 'bob@example.com', 25),
-    ('Charlie', 'charlie@example.com', 35)
-])
-
-kursor.execute(query)
-result = kursor.fetchall()
-
-tree = ttk.Treeview(m)
-
-tree['columns'] = ('Name', 'Email', 'Age')
-
-tree.heading('#0', text='Index')
-tree.heading('Name', text='Name')
-tree.heading('Email', text='Email')
-tree.heading('Age', text='Age')
-
-
-for row in result:
-    tree.insert('', 'end', text=row[0], values=row[1:])
-
-
-tree.pack(expand=True, fill='both')
 
 m.mainloop()
