99bdaa583c9b5bde198bced9c6099eea44734146
Eryk <eryk.nejfert@wp.pl>
2025-01-16 14:49:50 +0100

    db_struct improved and add table option

--- main.py
+++ main.py
@@ -10,35 +10,116 @@ import xml.etree.ElementTree as ET
 sqlite = None  
 kursor = None  
 
+
 def get_database_structure():
     global sqlite, kursor, label_db_struct
     
+    children = label_db_struct.winfo_children()
+    if len(children) > 2:
+        children[2].destroy() 
     
-    tree = ttk.Treeview(label_db_struct)
+    tree = ttk.Treeview(label_db_struct, xscrollcommand = h2.set, yscrollcommand = v2.set)
+    
     tree.pack(expand=True, fill="both")
+    columns = ["Type", "Schema"]
     
-    tree["columns"] = ["Type", "Schema"]
-    tree.heading("#0", text="Name")
-    tree.heading("Type", text="Type")
-    tree.heading("Schema", text="Schema")
+    tree["columns"] = columns
+    tree.heading("#0", text="Name", anchor='w')
+    tree.heading("Type", text="Type", anchor='w')
+    tree.heading("Schema", text="Schema", anchor='w')
+    
+    
+    longest_schema_length = 0
 
     
-    root_node = tree.insert("", "end", text="Database", open=True)
+    tree.tag_configure("underline", font=("", 9, "underline"))
+    
+    
+    table_node = tree.insert("", "end", text="Tables", open=True)
 
     query_table_names = "SELECT name, type, sql FROM sqlite_master WHERE type='table'"
     tables = sqlite.execute(query_table_names).fetchall()
     
-    print(tables)
     
     for table in tables:
         name = str(table[0])
-        schema = str(table[2]).replace("\n", "")
-        child_1 = tree.insert(root_node, "end", text=name, open=False, values=("", schema))
+        schema = str(table[2]).replace("\n", " ")
+        
+        if(longest_schema_length < len(schema)):
+            longest_schema_length = len(schema)
+        
+        child_1 = tree.insert(table_node, "end", text=name, open=False, values=("", schema))
         query_pragma = f"PRAGMA table_info({name});"
         table_struct = sqlite.execute(query_pragma).fetchall()
         for struct in table_struct:
-            tree.insert(child_1, "end", text=str(struct[1]), values=(str(struct[2]), ""))
+            result = f'"{struct[1]}" {struct[2]} {"NOT NULL " if struct[3] == 1 else ""} {"PRIMARY KEY" if struct[5] == 1 else ""}'
+            child_2 = tree.insert(child_1, "end", text=str(struct[1]), values=(str(struct[2]), result))
+            
+            if struct[5] == 1:
+                tree.item(child_2, tags=("underline",))
+            
+    view_node = tree.insert("", "end", text="Views", open=True)
+    
+    query_view_names = "SELECT name, type, sql FROM sqlite_master WHERE type='view'"
+    views = sqlite.execute(query_view_names).fetchall()
+    
+    for view in views:
+        name = str(view[0])
+        schema = str(view[2]).replace("\n", " ")
         
+        if(longest_schema_length < len(schema)):
+            longest_schema_length = len(schema)
+            
+        child_1 = tree.insert(view_node, "end", text=name, open=False, values=("", schema))
+        query_pragma = f"PRAGMA table_info({name});"
+        table_struct = sqlite.execute(query_pragma).fetchall()
+        for struct in table_struct:
+            result = f'"{struct[1]}" {struct[2]} {"NOT NULL " if struct[3] == 1 else ""} {"PRIMARY KEY" if struct[5] == 1 else ""}'
+            tree.insert(child_1, "end", text=str(struct[1]), values=(str(struct[2]), result))
+    
+    trigger_node = tree.insert("", "end", text="Triggers", open=True)
+    
+    query_triggers_names = "SELECT name, type, sql FROM sqlite_master WHERE type='trigger'"
+    triggers = sqlite.execute(query_triggers_names).fetchall()   
+    
+    for trigger in triggers:
+        name = str(trigger[0])
+        schema = str(trigger[2]).replace("\n", " ")
+        
+        if(longest_schema_length < len(schema)):
+            longest_schema_length = len(schema)
+            
+            
+        child_1 = tree.insert(trigger_node, "end", text=name, open=False, values=("", schema))
+          
+    tree.column("#0", width=max((int(m.winfo_width()/3)), 100), stretch=False)      
+    tree.column(columns[0], width=max((int(m.winfo_width()/3)), 100), stretch=False)      
+    tree.column(columns[1], width=max((longest_schema_length * 6), 100), anchor='w', stretch=False)      
+    
+    h2.config(command=tree.xview)
+    v2.config(command=tree.yview)    
+    
+    def on_right_click(event):
+        selected_item = tree.identify('item', event.x, event.y)
+        if selected_item == table_node:
+            tree.selection_set(selected_item)
+            menu = tkinter.Menu(m, tearoff=0)
+            menu.add_command(label="Add Table")
+            menu.post(event.x_root, event.y_root)
+        if selected_item == view_node:
+            tree.selection_set(selected_item)
+            menu = tkinter.Menu(m, tearoff=0)
+            menu.add_command(label="Add View")
+            menu.post(event.x_root, event.y_root)   
+        if selected_item == trigger_node:
+            tree.selection_set(selected_item)
+            menu = tkinter.Menu(m, tearoff=0)
+            menu.add_command(label="Add Trigger")
+            menu.post(event.x_root, event.y_root)      
+    
+    tree.bind("<Button-3>", on_right_click)
+    
+            
         
 
 def switch_tables(event):
@@ -58,11 +139,11 @@ def switch_tables(event):
     
 
     tree = ttk.Treeview(label_treedata,
-                 xscrollcommand = h.set, 
-                 yscrollcommand = v.set)
+                 xscrollcommand = h1.set, 
+                 yscrollcommand = v1.set)
     
-    h.config(command=tree.xview)
-    v.config(command=tree.yview)
+    h1.config(command=tree.xview)
+    v1.config(command=tree.yview)
     
     tree["columns"] = columns
     tree["show"] = "headings"
@@ -158,6 +239,9 @@ def import_db():
             label_treedata.pack(side='top', padx=0, pady=0, fill='both', expand=True)
             label_db_struct.config(bg='white')
             get_database_structure()
+            children = label_treedata.winfo_children()
+            if len(children) > 2:
+                children[2].destroy() 
         except sqlite3.Error as e:
             messagebox.showerror("Error", f"Error connecting to database: {e}")
             import_db()
@@ -249,10 +333,15 @@ label_db_struct.pack(side='left', fill='both', expand=True)
 label_browse_data = tkinter.Frame(m, padx=0, pady=0)
 label_treedata = tkinter.Frame(label_browse_data, bg="white", padx=0, pady=0)
 
-h = tkinter.Scrollbar(label_treedata, orient = 'horizontal')
-h.pack(side = BOTTOM, fill = X)
-v = tkinter.Scrollbar(label_treedata)
-v.pack(side = RIGHT, fill = Y)
+h1 = tkinter.Scrollbar(label_treedata, orient = 'horizontal')
+h1.pack(side = BOTTOM, fill = X)
+v1 = tkinter.Scrollbar(label_treedata)
+v1.pack(side = RIGHT, fill = Y)
+
+h2 = tkinter.Scrollbar(label_db_struct, orient = 'horizontal')
+h2.pack(side = BOTTOM, fill = X)
+v2 = tkinter.Scrollbar(label_db_struct)
+v2.pack(side = RIGHT, fill = Y)
 
 
 
